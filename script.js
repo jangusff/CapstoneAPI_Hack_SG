0a5067e40f3e553d585d6bd6d06df642265bd235
Jonathan Angus <jangusff@gmail.com>
2019-04-21 22:25:16 -0500

    added cast,character fetch and some styling

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ const endPtLocations = 'locations';
 const endPtSpecies = 'species';
 const endPtVehicles = 'vehicles';
 
-let filmDB = [];
-
+let FILM_DB = [];
+let CHARACTER_DB = {};
+let LASTVIEWED_ID = '';
 
 
 function setActiveAppPhase(targetPhase) {
@@ -44,6 +45,7 @@ function btnHndlr_EnterSite() {
   $('.intro-page').on('click', '.enter-site', event => {
     event.preventDefault();
     getFilmDatabase();
+    getCharacterDatabase();
   });
 }
 
@@ -52,7 +54,12 @@ function btnHndlr_BackToBrowse() {
   $('.details-view').on('click', '.back-to-browse-button', event => {
     event.preventDefault();
     setActiveAppPhase($('.browse-cards-page'));
-    scrollTo(0,0);
+    if (LASTVIEWED_ID !== "") {
+     let elmnt = document.getElementById(LASTVIEWED_ID)
+     elmnt.scrollIntoView();
+    } else {
+      scrollTo(0,0);
+    }
   });
 }
 
@@ -61,59 +68,24 @@ function btnHndlr_LearnMore() {
   $('.card-footer').on('click', '.card-button', event => {
     event.preventDefault();
     let targetID = $(event.target).parents('.card').attr('id');
-
-    let foundFilmObj = findById(filmDB, targetID);
-
-    if (jQuery.isEmptyObject(foundFilmObj)) {
-
-      alert("Object was empty.");
-
-    } else {
-      let filmTitle = foundFilmObj.title;
-      let filmRelease = foundFilmObj.release_date;
-      let filmDescr = foundFilmObj.description;
-      let filmVideoID = getVideoID(targetID);
-      
-      if (filmVideoID === "") {
-        // set class identifier such that some message like "Trailer not Available" displays instead of iframe stuff
-      }
-
-          
-      $('.details.feature-img').html(`<img src="images/${targetID}.jpg" alt="${filmTitle}">`);
-      $('.details.film-descrip').html(`<h2>Description</h2>
-          <p>${filmDescr}</p>`
-      );
-      $('.details.cast').html(`<h2>Characters</h2>
-          <ul>
-            <li>Suzuki Blondeau</li>
-            <li>Timo Potter</li>
-            <li>Kakashan Timoshi</li>
-          </ul>`
-      );
-      $('.details.trailer').html(`<h2>Play Trailer</h2>
-          <div class="iframe-container">
-            <iframe src="https://www.youtube.com/embed/${filmVideoID}?start=2&modestbranding=1&showinfo=0&rel=0&fs=0" frameborder="0"></iframe>
-          </div>
-          <button class="back-to-browse-button" type="submit">Back</button>`
-      );
-      /*
-            <iframe src="https://www.youtube.com/embed/${filmVideoID}?start=2&modestbranding=1&showinfo=0&rel=0&fs=0" width="450" height="300" frameborder="0"></iframe>
-      */
-      btnHndlr_BackToBrowse();
-      setActiveAppPhase($('.details-view'));
-    } // check for empty object
-
+    renderDetailsView(targetID);
   });
 }
 
 
-function displayFetchErr(err) {
+function filmDBFetchErr(err) {
   $('#initial-fetch-err').text(`Sorry. Unable to retrieve film information. Cannot proceed.`);
   if (err.length > 0) {
     $('#initial-fetch-err').append(`<p class='errmsg-display'>(Err: ${err})</p>`);
   }
   $('button.enter-site').addClass('button-hide');
 }
+
+function charDBFetchErr(err) {
+  // Since this DB is not critical, we'll implicitly silence this exception.
+  CHARACTER_DB = {};
+}
+
 
 function renderFilmCard(filmObj, index) {
   let filmID = filmObj.id;
@@ -147,26 +119,73 @@ function renderFilmCard(filmObj, index) {
   }
 }
 
-function displayBrowsePage(fetchedFilmDB) {
+function renderDetailsView(targetID) {
+    let foundFilmObj = findById(FILM_DB, targetID);
+    LASTVIEWED_ID = "";
+
+    if (jQuery.isEmptyObject(foundFilmObj)) {
+      alert("Sorry, no details available.");
+
+    } else {
+      let filmTitle = foundFilmObj.title;
+      let filmRelease = foundFilmObj.release_date;
+      let filmDescr = foundFilmObj.description;
+      let filmVideoID = getVideoID(targetID);
+      let castHtml = '';
+      let trailerHtml = '';
+
+      $('.details.feature-img').html(`<img src="images/${targetID}.jpg" alt="${filmTitle}">`);
+      $('.details.film-descrip').html(`<h2>Description</h2>
+          <p>${filmDescr}</p>`
+      );
+
+      if (CHARACTER_DB.hasOwnProperty(targetID)) {
+        let arrCast = CHARACTER_DB[targetID];
+        castHtml = `  <ul>`;
+        arrCast.forEach(oneName => { castHtml += `   <li>${oneName}</li>`});
+        castHtml += ' </ul>';
+      } else {
+        castHtml = `<p>(none)</p>`;
+      }
+
+      $('.details.cast').html(`<h2>Notable Characters</h2>${castHtml}`);
+      
+      if (filmVideoID !== "") {
+        trailerHtml = `<div class="iframe-container">
+              <iframe src="https://www.youtube.com/embed/${filmVideoID}?start=2&modestbranding=1&showinfo=0&rel=0&fs=0" frameborder="0"></iframe>
+            </div>`;
+      } else {
+        trailerHtml = `<p class="no trailer">No trailer found</p>`;
+      }
+
+      $('.details.trailer').html(`<h2>Play Trailer</h2>${trailerHtml}`);
+      
+      LASTVIEWED_ID = targetID;
+      setActiveAppPhase($('.details-view'));
+      scrollTo(0,0);
+
+    } // check for non-existent film object
+}
+
+
+function displayBrowsePage(fetchedFILM_DB) {
   let filmObj;
 
-  filmDB = fetchedFilmDB;
-  if (filmDB.length > 0 && filmDB.some(element2Chk => {
+  FILM_DB = fetchedFILM_DB;
+  if (FILM_DB.length > 0 && FILM_DB.some(element2Chk => {
     return Object.keys(element2Chk).length > 0;
   })) {
-    filmDB.forEach(renderFilmCard);
+    FILM_DB.forEach(renderFilmCard);
     btnHndlr_LearnMore();
     setActiveAppPhase($('.browse-cards-page'));
   } else {
-    displayFetchErr('Retrieved empty film database');
+    filmDBFetchErr('Retrieved empty film database');
   }
 }
 
 function getFilmDatabase() {
   const fieldsToIncl = 'fields=id,title,release_date,description';
   const url = baseURL + '/' + endPtFilms + '?' + fieldsToIncl;
-
-  //console.log(url);
 
   fetch(url)
     .then(response => {
@@ -176,12 +195,44 @@ function getFilmDatabase() {
       throw new Error(response.statusText);
     })
     .then(responseJson => displayBrowsePage(responseJson))
-    .catch(err => displayFetchErr(err.message));    
+    .catch(err => filmDBFetchErr(err.message));    
+}
+
+
+function getCharacterDatabase() {
+  const fieldsToIncl = 'fields=id,name,films';
+  const url = baseURL + '/' + endPtPeople + '?' + fieldsToIncl;
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => {
+      responseJson.forEach(apiRec => {
+        let name = apiRec.name;
+
+        for (let i = 0; i < apiRec.films.length; i++) {
+          let filmID = apiRec.films[i];
+          filmID = filmID.slice(filmID.lastIndexOf('/')+1);
+ 
+          if (CHARACTER_DB.hasOwnProperty(filmID)) {
+            CHARACTER_DB[filmID].push(name);
+          } else {
+            CHARACTER_DB[filmID] = [ name ];
+          }
+        }
+      });
+    })
+    .catch(err => charDBFetchErr(err.message));    
 }
 
 
 function formInit() {
   btnHndlr_EnterSite();
+  btnHndlr_BackToBrowse();
   
 }
 
