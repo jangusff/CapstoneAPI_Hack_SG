6eab8e4a6189465a8297deb9ed996eb87740d623
Jonathan Angus <jangusff@gmail.com>
2019-05-04 11:27:22 -0500

    Added comments to main Javascript script

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function setActiveAppPhase(targetPhase, showNav) {
   }
 }
 
+// Search our Film Database by ID and return its Object.
 function findById(arrToSrch, idNum) {
   let retObj = {};
   arrToSrch.forEach(itemObj => {
@@ -28,9 +29,10 @@ function findById(arrToSrch, idNum) {
       retObj = itemObj;
     }
   });
-    return retObj;
+  return retObj;
 }
 
+// Return the YouTube video ID for a given Film ID.
 function getVideoID(targetID) {
   let retVal = "";
   Object.keys(videoID).forEach(function(key) {
@@ -38,8 +40,10 @@ function getVideoID(targetID) {
       retVal = videoID[key];
     }
   });
-    return retVal;
+  return retVal;
 }
+
+// Error-handling functions...
 
 function filmDBFetchErr(err) {
   $('#initial-fetch-err').text(`Sorry. Unable to retrieve film information. Cannot proceed.`);
@@ -54,6 +58,34 @@ function charDBFetchErr(err) {
   CHARACTER_DB = {};
 }
 
+// Create the HTML necessary to display a single Film card.
+function htmlForCard(filmID, filmTitle, filmRelease) {
+  return `<div class="card" id="${filmID}">\
+            <img src="images/${filmID}.jpg" class="card-img-top" alt="${filmTitle}">\
+            <p class="card-release-date">(released ${filmRelease})</p>\
+            <div class="card-body">\
+              <h5 class="card-title">${filmTitle}</h5>\
+            </div>\
+            <div class="card-footer">\
+              <button class="card-button" type="submit">Learn More</button>\
+            </div>\
+          </div>`;
+}
+
+// Add a card to the Browse page of the site.
+function renderFilmCard(filmObj, index) {
+  let filmID = filmObj.id;
+  let filmTitle = filmObj.title;
+  let filmRelease = filmObj.release_date;
+    
+  if (index === 0) {
+    $('.app-phase.browse-cards-page').html(htmlForCard(filmID, filmTitle, filmRelease));
+  } else {
+    $('.card').last().after(htmlForCard(filmID, filmTitle, filmRelease));
+  }
+}
+
+// Iterate through the Film Database and formulate the Browse page.
 function displayBrowsePage(fetchedFILM_DB) {
   let filmObj;
 
@@ -69,79 +101,56 @@ function displayBrowsePage(fetchedFILM_DB) {
   }
 }
 
-function htmlForCard(filmID, filmTitle, filmRelease) {
-  return `<div class="card" id="${filmID}">\
-            <img src="images/${filmID}.jpg" class="card-img-top" alt="${filmTitle}">\
-            <p class="card-release-date">(released ${filmRelease})</p>\
-            <div class="card-body">\
-              <h5 class="card-title">${filmTitle}</h5>\
-            </div>\
-            <div class="card-footer">\
-              <button class="card-button" type="submit">Learn More</button>\
-            </div>\
-          </div>`;
-}
-
-function renderFilmCard(filmObj, index) {
-  let filmID = filmObj.id;
-  let filmTitle = filmObj.title;
-  let filmRelease = filmObj.release_date;
-    
-  if (index === 0) {
-    $('.app-phase.browse-cards-page').html(htmlForCard(filmID, filmTitle, filmRelease));
-  } else {
-    $('.card').last().after(htmlForCard(filmID, filmTitle, filmRelease));
-  }
-}
-
+// Create the Details page for a particular Film.
 function renderDetailsView(targetID) {
-    let foundFilmObj = findById(FILM_DB, targetID);
-    LASTVIEWED_ID = "";
+  let foundFilmObj = findById(FILM_DB, targetID);
+  LASTVIEWED_ID = "";
 
-    if (jQuery.isEmptyObject(foundFilmObj)) {
-      alert("Sorry, no details available.");
+  if (jQuery.isEmptyObject(foundFilmObj)) {
+    alert("Sorry, no details available.");
 
+  } else {
+    let filmTitle = foundFilmObj.title;
+    let filmRelease = foundFilmObj.release_date;
+    let filmDescr = foundFilmObj.description;
+    let filmVideoID = getVideoID(targetID);
+    let castHtml = '';
+    let trailerHtml = '';
+
+    $('.details.feature-img').html(`<img src="images/${targetID}.jpg" alt="${filmTitle}">`);
+    $('.details.film-descrip').html(`<h2>Description</h2>
+        <p>${filmDescr}</p>`
+    );
+
+    if (CHARACTER_DB.hasOwnProperty(targetID)) {
+      let arrCast = CHARACTER_DB[targetID];
+      castHtml = `  <ul>`;
+      arrCast.forEach(oneName => { castHtml += `   <li>${oneName}</li>`});
+      castHtml += ' </ul>';
     } else {
-      let filmTitle = foundFilmObj.title;
-      let filmRelease = foundFilmObj.release_date;
-      let filmDescr = foundFilmObj.description;
-      let filmVideoID = getVideoID(targetID);
-      let castHtml = '';
-      let trailerHtml = '';
+      castHtml = `<p>(none)</p>`;
+    }
 
-      $('.details.feature-img').html(`<img src="images/${targetID}.jpg" alt="${filmTitle}">`);
-      $('.details.film-descrip').html(`<h2>Description</h2>
-          <p>${filmDescr}</p>`
-      );
+    $('.details.cast').html(`<h2>Notable Characters</h2>${castHtml}`);
+    
+    if (filmVideoID !== "") {
+      trailerHtml = `<div class="iframe-container">
+            <iframe src="https://www.youtube.com/embed/${filmVideoID}?start=2&modestbranding=1&showinfo=0&rel=0&fs=0" frameborder="0"></iframe>
+          </div>`;
+    } else {
+      trailerHtml = `<p class="no trailer">No trailer found</p>`;
+    }
 
-      if (CHARACTER_DB.hasOwnProperty(targetID)) {
-        let arrCast = CHARACTER_DB[targetID];
-        castHtml = `  <ul>`;
-        arrCast.forEach(oneName => { castHtml += `   <li>${oneName}</li>`});
-        castHtml += ' </ul>';
-      } else {
-        castHtml = `<p>(none)</p>`;
-      }
+    $('.details.trailer').html(`<h2>Play Trailer</h2>${trailerHtml}`);
+    
+    LASTVIEWED_ID = targetID;
+    setActiveAppPhase($('.details-view'), true);
+    scrollTo(0,0);
 
-      $('.details.cast').html(`<h2>Notable Characters</h2>${castHtml}`);
-      
-      if (filmVideoID !== "") {
-        trailerHtml = `<div class="iframe-container">
-              <iframe src="https://www.youtube.com/embed/${filmVideoID}?start=2&modestbranding=1&showinfo=0&rel=0&fs=0" frameborder="0"></iframe>
-            </div>`;
-      } else {
-        trailerHtml = `<p class="no trailer">No trailer found</p>`;
-      }
-
-      $('.details.trailer').html(`<h2>Play Trailer</h2>${trailerHtml}`);
-      
-      LASTVIEWED_ID = targetID;
-      setActiveAppPhase($('.details-view'), true);
-      scrollTo(0,0);
-
-    } // check for non-existent film object
+  } // check for non-existent film object
 }
 
+// Query API for Film information and build our Browse page.
 function getFilmDatabase() {
   const fieldsToIncl = 'fields=id,title,release_date,description';
   const url = baseURL + '/' + endPtFilms + '?' + fieldsToIncl;
@@ -157,6 +166,7 @@ function getFilmDatabase() {
     .catch(err => filmDBFetchErr(err.message));    
 }
 
+// Query API for Film Character information. Build our Character database from the data returned.
 function getCharacterDatabase() {
   const fieldsToIncl = 'fields=id,name,films';
   const url = baseURL + '/' + endPtPeople + '?' + fieldsToIncl;
@@ -187,6 +197,7 @@ function getCharacterDatabase() {
     .catch(err => charDBFetchErr(err.message));    
 }
 
+// Button on intro page; brings user to Browse page when clicked.
 function btnHndlr_EnterSite() {
   $('.intro-page').on('click', '.enter-site', event => {
     event.preventDefault();
@@ -198,6 +209,7 @@ function btnHndlr_EnterSite() {
   });
 }
 
+// Set up Nav bar link to 'Home' (Intro page).
 function navHndlr_BackToHome() {
   $('nav').on('click', '.nav-home', event => {
     event.preventDefault();
@@ -205,6 +217,7 @@ function navHndlr_BackToHome() {
   });
 }
 
+// Set up Nav bar link to 'Back to Browse' (Browse Films page).
 function navHndlr_BackToBrowse() {
   $('nav').on('click', '.nav-browse', event => {
     event.preventDefault();
@@ -218,6 +231,7 @@ function navHndlr_BackToBrowse() {
   });
 }
 
+// Button shown at bottom of 'Details' page. Returns user to 'Browse' page when clicked.
 function btnHndlr_BackToBrowse() {
   $('.details-view').on('click', '.back-to-browse-button', event => {
     event.preventDefault();
@@ -231,6 +245,7 @@ function btnHndlr_BackToBrowse() {
   });
 }
 
+// Button shown at the bottom of an individual card that's displayed in the 'Browse' page.
 function btnHndlr_LearnMore() {
   $('.card-footer').on('click', '.card-button', event => {
     event.preventDefault();
